e7b29058f275be43f4f8178121b563e9fa67acec
Muhammad Aditya <81363093+aditya3232@users.noreply.github.com>
2024-03-11 20:04:43 +0700

    add implementasi interface di repository

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -12,9 +12,9 @@ import (
 */
 
 type CategoryRepository interface {
-	Create(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
-	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
-	FindById(ctx context.Context, tx sql.Tx, categoryId int) domain.Category
-	FindAll(ctx context.Context, tx sql.Tx) []domain.Category
+	Create(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
+	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
